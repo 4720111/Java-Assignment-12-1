32f7f5d6ddb96152be100c792dbae8b19936ce00
4720111 <103705321+4720111@users.noreply.github.com>
2022-07-08 10:35:03 +0900

    Update DNA.java

--- src/main/java/DNA.java
+++ src/main/java/DNA.java
@@ -1,3 +1,26 @@
-public class DNA {
 
+/**
+ * クラス DNA の注釈をここに書きます.
+ * 
+ * @author (片岡小雪)
+ * @version (2022/07/08)
+ */
+public class DNA {
+    String str = "";
+    String str1 = "";
+    public String getPair(){
+        for(int i = 0;i<this.str.length();i++){
+            switch(str.charAt(i)){
+                case 'A': str1 += "T";
+                break;
+                case 'T': str1 += "A";
+                break;
+                case 'C': str1 += "G";
+                break;
+                case 'G': str1 += "C";
+                break;
+            }
+        }
+        return str1;
+    }
 }
